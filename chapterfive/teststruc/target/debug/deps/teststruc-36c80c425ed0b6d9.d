eba55b10edc483c769ee30da694749e33265009d
bloodmoonfire <bluemoonfire87@gmail.com>
2022-08-15 19:55:22 -0500

    Chapter6Exaamples

---
+++ chapterfive/teststruc/target/debug/deps/teststruc-36c80c425ed0b6d9.d
@@ -0,0 +1,5 @@
+/Users/blood/scripts/rust/chapterfive/teststruc/target/debug/deps/teststruc-36c80c425ed0b6d9: src/main.rs
+
+/Users/blood/scripts/rust/chapterfive/teststruc/target/debug/deps/teststruc-36c80c425ed0b6d9.d: src/main.rs
+
+src/main.rs:
---
+++ chapterfive/teststruc/target/debug/teststruc.d
@@ -0,0 +1,1 @@
+/Users/blood/scripts/rust/chapterfive/teststruc/target/debug/teststruc: /Users/blood/scripts/rust/chapterfive/teststruc/src/main.rs
